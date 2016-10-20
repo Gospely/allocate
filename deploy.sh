138bd8020a461fcad99b32abb67bacc3ded9f390
leinue <597055914@qq.com>
2016-10-20 08:54:38 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker stop gospel_socket && docker rm gospel_socket
 docker build -t gospel_socket .
-docker run --rm -it -p 2333:3000 -p 2334:22 -w /root/workspace -v /root/workspace/:/var/www/storage/codes/vue-f7 -v /var/www/gospely/socket:/root/.gospely/.socket --name="gospel_socket" gospel_socket
+docker run --rm -it -p 2333:3000 -p 2334:22 -w /root/workspace -v /var/www/storage/codes/vue-f7:/root/workspace/ -v /var/www/gospely/socket:/root/.gospely/.socket --name="gospel_socket" gospel_socket
