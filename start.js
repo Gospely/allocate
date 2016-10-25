65be217bf46f8e5bce9ed30dd652afcb4fd5e564
apple <937257166@qq.com>
2016-10-25 15:34:54 +0800

    update

--- start.js
+++ start.js
@@ -50,13 +50,14 @@ var name = argv.n,
     sshPort = argv.s,
     socketResource = argv.r,
     appPort = argv.a,
-    password = argv.w;
+    password = argv.w,
+    memory = argv.m;
 
 if(socketResource == null || socketResource == undefined || socketResource == ''){
   socketResource = "/var/www/gospely/socket";
 }
 
-var runBash = 'docker build -t gospel_socket . && docker run -itd -v /var/www/storage/codes/' + name + ':/root/workspace/' + name + ' -p ' + port + ':3000 -p ' + appPort + ':8086 -p ' + sshPort + ':22 -w /root/.gospely/.socket -v ' + socketResource + ':/root/.gospely/.socket --name="gospel_project_' + name + '" gospel_socket && docker exec  gospel_project_' + name +' && echo "root:' +password+ '"| chpasswd';
+var runBash = 'docker build -t gospel_socket . && docker run -itd -v /var/www/storage/codes/' + name + ':/root/workspace/' + name + ' -m '+ memory +'  -p ' + port + ':3000 -p ' + appPort + ':8086 -p ' + sshPort + ':22 -w /root/.gospely/.socket -v ' + socketResource + ':/root/.gospely/.socket --name="gospel_project_' + name + '" gospel_socket && docker exec  gospel_project_' + name +' && echo "root:' +password+ '"| chpasswd';
 console.log(runBash);
 var result = exec(runBash);
 
