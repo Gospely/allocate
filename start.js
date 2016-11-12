ce495abb1eb0bf12c8c4915bd320e5bc2fc0cc65
SharksevenRo <937257166@qq.com>
2016-11-12 15:59:50 +0800

    update

--- start.js
+++ start.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
-//
+ //
 require('shelljs/global');
 var argv = require('yargs')
   .option('n', {
-    alias : 'name',
+    alias: 'name',
     demand: false,
     describe: 'container name',
     type: 'string'
   })
   .option('p', {
-    alias : 'port',
+    alias: 'port',
     demand: false,
     describe: 'container port to be exposed',
   })
   .option('s', {
-    alias : 'sshport',
+    alias: 'sshport',
     demand: false,
     describe: 'container ssh port to be exposed',
   })
   .option('r', {
-    alias : 'resource',
+    alias: 'resource',
     demand: false,
     describe: 'socket resource',
   })
@@ -43,29 +43,36 @@ var argv = require('yargs')
     demand: true,
     describe: 'docker file path'
   })
+  .option('o', {
+    alias: 'host',
+    demand: true,
+    describe: 'host name'
+  })
   .usage('Usage: start.js [options]')
-  .example('start.js -n foo -p 7100 -s 8888 -r /var/www/gospely/socket', 'run Gospel socket')
+  .example('start.js -n foo -p 7100 -s 8888 -r /var/www/gospely/socket',
+    'run Gospel socket')
   .help('h')
   .alias('h', 'help')
   .epilog('Gospely Copyright 2016')
   .argv;
 
 var name = argv.n,
-    port = argv.p,
-    sshPort = argv.s,
-    socketResource = argv.r,
-    appPort = argv.a,
-    password = argv.w,
-    memory = argv.m,
-    filePath = argv.f;
-    console.log(filePath);
+  port = argv.p,
+  sshPort = argv.s,
+  socketResource = argv.r,
+  appPort = argv.a,
+  password = argv.w,
+  memory = argv.m,
+  filePath = argv.f,
+  hostName = argv.o;
+console.log(filePath);
 var imageName = filePath;
-if(filePath == 'c++'){
+if (filePath == 'c++') {
   imageName == cpp;
 }
 var split = filePath.split(":");
 
-if(split[1] != "lastest") {
+if (split[1] != "lastest") {
   filePath = split[0] + "/" + split[1];
 }
 //var cloneCmd = " && git clone " + git + "  /var/www/storage/codes/" + name +" && rm -rf .git"
@@ -73,18 +80,25 @@ if(split[1] != "lastest") {
 // if(imageName == 'nodejs' || imageName == 'vue-f7'){
 //   imageName = 'socket';
 // }
-if(socketResource == null || socketResource == undefined || socketResource == ''){
+if (socketResource == null || socketResource == undefined || socketResource ==
+  '') {
   socketResource = "/var/www/gospely/socket";
 }
 var sshCmd = "echo 'root:" + password + "' | chpasswd ";
-var runBash = 'docker build -t gospel_' + imageName + ' /root/gospely/allocate/df/' + filePath  +' && docker run -itd -v /var/www/storage/codes/' + name + ':/root/workspace/' + name + ' -m '+ memory +'  -p ' + port + ':3000 -p ' + appPort + ':8086 -p ' + sshPort + ':22 -w /root/.gospely/.socket -v ' + socketResource + ':/root/.gospely/.socket --name="gospel_project_' + name + '"  gospel_' + imageName;
+var runBash = 'docker build -t gospel_' + imageName +
+  ' /root/gospely/allocate/df/' + filePath +
+  ' && docker run -itd -v /var/www/storage/codes/' + name + ':/root/workspace/' +
+  name + ' -m ' + memory + '  -p ' + port + ':3000 -p ' + appPort + ':8086 -p ' +
+  sshPort + " -h " + hostName + ':22 -w /root/workspace -v ' + socketResource +
+  ':/root/.gospely/.socket --name="gospel_project_' + name + '"  gospel_' +
+  imageName;
 console.log(runBash);
 var result = exec(runBash);
 cd
 
 
-if(result.code !== 0) {
-   console.error(result);
-}else {
-   console.log('Gosple Container is running, SSH: ' + sshPort, 'Socket: ' + port);
+if (result.code !== 0) {
+  console.error(result);
+} else {
+  console.log('Gosple Container is running, SSH: ' + sshPort, 'Socket: ' + port);
 }
